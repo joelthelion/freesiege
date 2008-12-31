5bfb6cb258bb526f438b074bbe024c835f153bc4
Joel Schaerer <joel.schaerer@laposte.net>
2008-12-31 15:17:55 +0100

    make default level easier

--- gui/trainingscreen.cpp
+++ gui/trainingscreen.cpp
@@ -23,7 +23,7 @@
 #include "param.h"
 
 #define FONT_COLOR { 0x77, 0xd1, 0x00, 0 }
-#define LEVEL1_SPEED 100
+#define LEVEL1_SPEED 150
 #define SURVIVAL_TIME 3000
 
 TrainingScreen::TrainingScreen(const SpriteCollection *spr_coll,const CombinaisonCollection *cmb_coll,const std::string &ttf_path,TextureIds ids,Background *background) {
